bbd9bdf942e505febdb5d22e3e50763ef4685eb2
Abhinav <abhi32eee@gmail.com>
2023-03-05 10:42:41 +0530

    built the function for the help command

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ client.on("ready", () => {
 });
 
 /*
+ Returns all commands available for this discord bot
+
+*/
+client.on("messageCreate",(message) => {
+  if(message.content.toLowerCase() === "help"){
+    const allCommands = ["random","easy","medium","hard","notify"];
+    let helpMsg = "To get started with using this bot, type any of the following commands" + "\n";
+    allCommands.forEach((command,index) => {
+      helpMsg += `**${index + 1}) ${command}**`;
+      helpMsg += "\n";
+    });
+    console.log(helpMsg);
+    message.reply(helpMsg);
+  }
+});
+/*
  Returns a random problem from leetcode when the message "random" is typed 
 */
 client.on("messageCreate", async (message) => {
